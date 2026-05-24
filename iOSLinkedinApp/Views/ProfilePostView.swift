379d633aa57aa31dfa904f585db442a30eb45a36
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-24 11:38:33 +0530

    profile post view

---
+++ iOSLinkedinApp/Views/ProfilePostView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePostView.swift
+//  iOSLinkedinApp
+//
+//  Created by Arjun Saseendran on 24/05/26.
+//
+
+import SwiftUI
+
+struct ProfilePostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePostView()
+}
