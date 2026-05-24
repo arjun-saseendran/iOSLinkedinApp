f4477f770c991e89bd4041e3b3558a5d741187f9
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-24 07:40:03 +0530

    Initial Commit

---
+++ iOSLinkedinApp/iOSLinkedinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSLinkedinAppApp.swift
+//  iOSLinkedinApp
+//
+//  Created by Arjun Saseendran on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSLinkedinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
