ece066172aae6174adfb1258c88d9702960fe91c
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-24 08:45:52 +0530

    add serach bar view

---
+++ iOSLinkedinApp/Views/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  iOSLinkedinApp
+//
+//  Created by Arjun Saseendran on 24/05/26.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
