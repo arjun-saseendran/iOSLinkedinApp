d6029d894f4e706af1d37335a2b749562c5ac72a
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-24 12:38:16 +0530

    add home screen

---
+++ iOSLinkedinApp/Views/TabScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  TabScreenView.swift
+//  iOSLinkedinApp
+//
+//  Created by Arjun Saseendran on 24/05/26.
+//
+
+import SwiftUI
+
+struct TabScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabScreenView()
+}
