bc7d3719aa6e9b14a46baa6b01d58dcfed674510
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-24 11:27:20 +0530

    add post card view

---
+++ iOSLinkedinApp/Views/PostCardView.swift
@@ -0,0 +1,18 @@
+//
+//  PostCardView.swift
+//  iOSLinkedinApp
+//
+//  Created by Arjun Saseendran on 24/05/26.
+//
+
+import SwiftUI
+
+struct PostCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostCardView()
+}
