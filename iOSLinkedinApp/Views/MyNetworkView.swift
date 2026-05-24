e826777d4215544cb47076934558c8922179f36e
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-24 10:28:15 +0530

    add my network view

---
+++ iOSLinkedinApp/Views/MyNetworkView.swift
@@ -0,0 +1,18 @@
+//
+//  MyNetworkView.swift
+//  iOSLinkedinApp
+//
+//  Created by Arjun Saseendran on 24/05/26.
+//
+
+import SwiftUI
+
+struct MyNetworkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyNetworkView()
+}
