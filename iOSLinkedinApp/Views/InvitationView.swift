cf09dbf77dac2079abfa0ef9feed51e56fa59d25
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-24 09:29:15 +0530

    add invitation view

---
+++ iOSLinkedinApp/Views/InvitationView.swift
@@ -0,0 +1,18 @@
+//
+//  InvitationView.swift
+//  iOSLinkedinApp
+//
+//  Created by Arjun Saseendran on 24/05/26.
+//
+
+import SwiftUI
+
+struct InvitationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InvitationView()
+}
